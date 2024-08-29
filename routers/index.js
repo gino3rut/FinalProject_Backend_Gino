24a45a765e8498af78e2aa06a0ca9ab80e4744a2
gino3rut <gino.sunarto@yahoo.com>
2024-08-29 17:22:05 +0700

    second commit

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,5 @@
 const router = require('express').Router();
 
-const BookmarksController = require('../controllers/BookmarksController.js');
-
-router.post('/bookmark/:id', BookmarksController.createBookmarks);
-
 router.use('/users', require("./users"));
 router.use(require('../middlewares/authentication.js'));
 router.use('/movies', require("./movies"));
